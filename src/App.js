53b0c06641bc20b9d3dc42565c7cda4a96f12775
LE TAN LOC <letanlocjp1001@gmail.com>
2022-02-19 23:51:25 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 function App() {
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select data-choose-theme>
+        <option value='dark'>Dark</option>
+        <option value='light'>Light</option>
+        <option value='dracula'>Dracula</option>
+        <option value='valentine'>valentine</option>
+      </select>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
